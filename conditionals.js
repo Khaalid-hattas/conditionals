e68a1496bdd2fe43f82d1d666caccd928f121ec1
likhonamkhatshana <likhonamkhatshana10@gmail.com>
2026-05-25 03:41:46 -0700

    Update conditionals.js

--- conditionals.js
+++ conditionals.js
@@ -1,7 +1,7 @@
 
-let score = 101;
+let score = 101;     
 
-let passingGrade = 60;
+let passingGrade = 50;
 
 if (score === 100) {
     console.log("Perfect score!");
@@ -19,4 +19,14 @@ if (score > 100) {
 
 }
 
+//Likhona 
+//Likhona created the else statement
+//Likhona created the failed print result
+//Likhona created the condition for perfect score
+//Likhona created the print result based off a perfect score
 
+//Khaalid
+//Khaalid created the variable called score and gave it a value between 0-100
+//Khaalid created the variable called  passingGrade and set it to 50
+//Khaalid created the if statement that checks if the score greater or equal to the passingGrade 
+// both of us worked on the if greater than 100 = to (you have exceeded the pass requirements)
