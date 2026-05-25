e5143921ba92632b32926c8de9cf4ac34572b8e1
Khaalid-hattas <161817858+Khaalid-hattas@users.noreply.github.com>
2026-05-25 11:24:41 +0100

    Add files via upload

---
+++ conditionals.js
@@ -0,0 +1,14 @@
+
+let score = 85;
+
+let passingGrade = 60;
+
+if (score === 100) {
+    console.log("Perfect score!");
+}
+
+if (score >= passingGrade) {
+    console.log("Passed");
+} else {
+    console.log("Failed");
+} 
